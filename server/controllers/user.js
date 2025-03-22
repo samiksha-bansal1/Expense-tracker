030649f644cbc8c8fb84b4eed577422a3d4c97b9
samiksha-bansal1 <samikshabansal2005@gmail.com>
2025-03-22 15:56:58 +0530

    updated-user-routes

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -7,13 +7,13 @@ const JWT_SECRET = "secretkey1";
 const handleSignin = async (req, res) => {
   try {
     const { name, email, password } = req.body;
-    console.log(req.body);
+    // console.log(req.body);
     let user = await User.findOne({ email });
     if (user) return res.status(400).json({ message: "User already exists" });
 
     user = new User({ name, email, password });
     await user.save();
-    console.log(user);
+    // console.log(user);
     res.status(201).json({ message: "User registered successfully" });
   } catch (err) {
     res.status(500).json({ message: "Server Error" });
@@ -23,15 +23,15 @@ const handleSignin = async (req, res) => {
 const handleLogin = async (req, res) => {
   try {
     const { email, password } = req.body;
-    console.log(email, password);
+    // console.log(email, password);
     let user = await User.findOne({ email });
-    console.log(user);
+    // console.log(user);
     if (!user) return res.status(400).json({ message: "Invalid credentials" });
 
     const isMatch = await bcrypt.compare(password, user.password);
-    console.log("Entered Password:", password);
-    console.log("Stored Hashed Password:", user.password);
-    console.log("Password Match:", isMatch);
+    // console.log("Entered Password:", password);
+    // console.log("Stored Hashed Password:", user.password);
+    // console.log("Password Match:", isMatch);
     if (!isMatch)
       return res.status(400).json({ message: "Invalid credentials" });
 
@@ -54,8 +54,27 @@ const handleLogout = async (req, res) => {
   res.clearCookie("token").json({ message: "Logged out successfully" });
 };
 
+const handleUserDetails = async (req, res) => {
+  try {
+    if (!req.user) {
+      return res.status(401).json({ message: "Unauthorized: No User Found" });
+    }
+
+    const user = await User.findById(req.user.id).select("-password");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json(user);
+  } catch (error) {
+    console.error("Error fetching user details:", error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
 module.exports = {
   handleSignin,
   handleLogin,
   handleLogout,
+  handleUserDetails,
 };
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,11 +3,15 @@ const {
   handleSignin,
   handleLogin,
   handleLogout,
+  handleUserDetails,
 } = require("../controllers/user");
+const {
+  checkForAuthenticationInCookie,
+} = require("../middlewares/authentication");
 
 const router = express.Router();
 router.post("/signin", handleSignin);
 router.post("/login", handleLogin);
 router.post("/logout", handleLogout);
-
+router.get("/details", checkForAuthenticationInCookie, handleUserDetails);
 module.exports = router;
